aeb6dcd172f16a429b9dde23b0bb0f5b39c73c87
Alice <contact@aliceq.me>
2015-10-20 11:06:25 -0400

    Added transparency to lines between nodes, added 3-note chords

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ var mouseMax = 40;
 var mouseStep = 3;  
 
 var mSmooth = .5;   // Mouse smoothing
-var smoothHigh = .9; // Smoothing factor low-high
+var smoothHigh = .95; // Smoothing factor low-high
 var smoothLow = .99;  // Smoothing factor high-low
 
 var noiseAmp = 0.75;  // Noise amplitude
@@ -20,10 +20,12 @@ var nodeBaseline = 100; // Lowest radius of a node
 var nodeScale = 0.5;   // Scale factor of a node's size
 
 var keyRange = 1;     // Local range of notes
+var chordPercent = .5;  // Percentage of notes to play as chords
 
 var bpm = 120;        // Beats to play per minute
 // C# harmonic minor
 var scale;
+var chords;
 
 // Color
 
@@ -60,14 +62,21 @@ createCanvas(windowWidth, windowHeight);
 background(bgIdleColor);
 // Create noise
 noise = new p5.Noise();
-noise.setType('pink');
+noise.setType('white');
 noise.amp(0);
 noise.start();
 
 // Config
-scale = [277, 311, 330, 370, 415, 440, 523, 554, 622, 660, 740, 830];
+
+//       C#4  D#4  E4   F#4  G#4  A4   C5   C#5  D#5  E5   F#5  G#5  A5
+//        0    1    2    3    4    5    6    7    8    9    10   11   12
+scale = [277, 311, 330, 370, 415, 440, 523, 554, 622, 660, 740, 830, 880];
+chord = [0, 2, 4];    // Shape of a chord
+
 noStroke();
-noCursor();
+cursor(HAND);
+
+strokeWeight(3);
 
 // Init
 notepX = mouseX;
@@ -104,7 +113,11 @@ var note = 2 * beat / pow(2, n);
 mNext = millis() + note;
 
 // Create note
+if (random(1) < chordPercent){
+  createChord(note * 4, 1 - sq(1 - epsilon/255));
+}else{
 createNote(note * 4, 1 - sq(1 - epsilon/255));
+}
 }
 
 // Noise amplitude
@@ -127,12 +140,11 @@ notes[i].update();
 
 // Draw line between remaining nodes
 colorMode(HSB);
-strokeWeight(2);
 for (var i = 1; i < notes.length; i++){
   var n0 = notes[i-1];
   var n1 = notes[i];
 
-  stroke(n0.hue, 50, 128);
+  stroke(n0.hue, 50, 128, sq(epsilon/255));
 
   line(n0.xPos, n0.yPos, n1.xPos, n1.yPos);
 }
@@ -142,7 +154,25 @@ hue = (hue + 0.1) % 255;
 }
 
 function mouseClicked(){
-  createNote(1000, 1);
+  createChord(1000, 1);
+}
+
+function createChord(duration, amplitude){
+// Get a random root
+var root = floor(random(scale.length - chord[chord.length - 1]));
+
+// Create the chord from the root
+for (var i = 0; i < chord.length; i++){
+  // Find frequency
+  var f = scale[root + chord[i]];
+  // Cerate new note
+  var n = new note(f, duration, mouseX, mouseY);
+  // Initialize it
+  n.start(amplitude);
+  // Push to stack
+  notes.push(n);
+}
+
 }
 
 function createNote(duration, amplitude){
@@ -182,18 +212,18 @@ this.end = millis() + duration;
 this.hue = floor(hue);
 
 this.frequency = frequency;
-this.scale = nodeBaseline + nodeScale * (scale[scale.length - 1] - frequency);
-
-console.log(this.scale);
+this.scale = nodeBaseline + nodeScale * (scale[scale.length - 1] - frequency);low-high
 
 this.completed = function(){ return (millis() >= this.end); }
 }
 
+// Note initialization method, called once
 note.prototype.start = function(amplitude){
   this.env.mult(amplitude);
   this.env.play(this.osc);
 }
 
+// Note cleanup method, called once
 note.prototype.stop = function(){
   this.osc.stop();
   this.osc.disconnect();
@@ -202,6 +232,7 @@ note.prototype.stop = function(){
   delete(this.osc);
 }
 
+// Main update method, called once per draw
 note.prototype.update = function(){
   var p = (this.end - millis()) / this.duration;
   var localScale = this.scale * p * 0.5;
@@ -209,34 +240,34 @@ note.prototype.update = function(){
   this.xPos += this.vX;
   this.yPos += this.vY;
 
-  // Bounce
-  if (this.xPos < localScale){ 
-    this.xPos = localScale;
-    this.vX *= -1;
-  }else if (this.xPos > windowWidth - localScale){ 
-    this.xPos = windowWidth - localScale;
-    this.vX *= -1;
-  }
+// Bounce
+if (this.xPos < localScale){ 
+  this.xPos = localScale;
+  this.vX *= -1;
+}else if (this.xPos > windowWidth - localScale){ 
+  this.xPos = windowWidth - localScale;
+  this.vX *= -1;
+}
 
-  if (this.yPos < localScale){ 
-    this.yPos = localScale;  
-    this.vY *= -1;
-  }else if (this.yPos > windowHeight - localScale){ 
-    this.yPos = windowHeight - localScale;
-    this.vY *= -1;
-  }
+if (this.yPos < localScale){ 
+  this.yPos = localScale;  
+  this.vY *= -1;
+}else if (this.yPos > windowHeight - localScale){ 
+  this.yPos = windowHeight - localScale;
+  this.vY *= -1;
+}
 
-  // Pan sound
-  var pan = -1 + ((this.xPos - localScale) / (windowWidth - 2 *localScale)) * 2;
-  if (pan >= 0){
-    pan = sqrt(pan);
-  }else{
-    pan = -sqrt(-pan);
-  }
-  this.osc.pan(pan);
+// Pan sound
+var pan = -1 + ((this.xPos - localScale) / (windowWidth - 2 *localScale)) * 2;
+if (pan >= 0){
+  pan = sqrt(pan);
+}else{
+  pan = -sqrt(-pan);
+}
+this.osc.pan(pan);
 
-  // Draw sphere
-  colorMode(HSB);
-  fill(this.hue, 50, 20 + 200 * p, sq(epsilon/255));
-  ellipse(this.xPos, this.yPos, localScale * 2, localScale * 2);
+// Draw sphere
+colorMode(HSB);
+fill(this.hue, 50, 20 + 200 * p, sq(epsilon/255));
+ellipse(this.xPos, this.yPos, localScale * 2, localScale * 2);
 }
